c3cca5221bd3ed6574af65d6578f6bf083869fc1
mNiechcial <36309995+mNiechcial@users.noreply.github.com>
2019-12-06 13:53:51 +0100

    ejs implementation

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ import { render } from 'pug';
 import expressHbs from 'express-handlebars';
 
 const app = express();
-app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
-app.set('view engine', 'pug');//global define
+//app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
+app.set('view engine', 'ejs');//global define
 app.set('views', 'views')//not required, default route
 app.use(urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,6 +17,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render(404, {pageTitle: "Not Found"});
+    res.status(404).render('notFound', {path: '', pageTitle: "Not Found"});
 })
 app.listen(3000);
