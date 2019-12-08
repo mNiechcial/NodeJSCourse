8570585aa112db6fedbed33870262e501aa47427
mNiechcial <36309995+mNiechcial@users.noreply.github.com>
2019-12-08 13:38:42 +0100

    dummy user, create user association

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ import shopRoutes from "./routes/shop";
 import rootDir from "./util/path";
 import path from "path";
 import errorController from "./controllers/errorController";
+import { request } from "http";
 
-const sequelize = require('./util/database')
-
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+//const Cart = require('./models/cart')
+const User = require("./models/user");
 const app = express();
 //app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
 app.set("view engine", "ejs"); //global define
@@ -17,18 +20,38 @@ app.use(urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
+sequelize
+    .sync()
     .then(result => {
+        return User.findByPk(1);
+        //app.listen(3000);
+    })
+    .then(user => {
+        if (!user) {
+            User.create({ name: "Miki", email: "test@test.com" });
+        }
+        return user;
+    })
+    .then(user => {
         app.listen(3000);
     })
     .catch(err => {
-        console.log(err)
+        console.log(err);
     });
-
-
-
